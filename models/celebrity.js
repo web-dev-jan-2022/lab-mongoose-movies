eb105c039ead1019ef93e9a8883ea1b28a94fc92
breogann <fernandocostapradillo@gmail.com>
2023-01-04 13:21:49 +0100

    uploading

---
+++ models/celebrity.js
@@ -0,0 +1,7 @@
+// Create the celebrity schema with the properties name, occupation and catchPhrase.
+// Create the Celebrity model with the schema.
+// Export the Celebrity model.
+
+const mongoose = require('mongoose');
+const celebritySchema = new mongoose.Schema({});
+const Celebrity = mongoose.model('Celebrity, ')
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const app = require('./app');
+const mongoose = require("mongoose");
+const app = require("./app");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
@@ -9,16 +9,16 @@ const PORT = process.env.PORT;
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log('Mongoose connection established.');
+    console.log("Mongoose connection established.");
     const server = app.listen(PORT, () => {
       console.log(`Listening on http://localhost:${PORT}`);
     });
-    server.on('error', (error) => {
-      if (error.syscall !== 'listen') {
+    server.on("error", (error) => {
+      if (error.syscall !== "listen") {
         throw error;
       }
       switch (error.code) {
-        case 'EADDRINUSE':
+        case "EADDRINUSE":
           console.error(`Port ${PORT} is already in use`);
           process.exit(1);
           break;
@@ -29,7 +29,7 @@ mongoose
   })
   .catch((error) => {
     console.error(
-      `There was an error connecting the database to URI "${URI}"`,
+      `There was an error connecting the database to URI ${MONGODB_URI}`,
       error
     );
   });
